e270c57f2103e04093d4526db84252614e0848ef
Lucio <luciomoraes@gmail.com>
2024-10-07 23:36:24 +0200

    creating views

--- main.go
+++ main.go
@@ -2,30 +2,25 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/luciormoraes/my-go-gallery/views"
 )
 
 func executeTemplate(w http.ResponseWriter, filePath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join(filePath)
-	tpl, err := template.ParseFiles(tplPath)
+
+	t, err := views.Parse(filePath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
 
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
+
 }
 
 func homeHandlerFunc(w http.ResponseWriter, r *http.Request) {
