162f8066d3072ec206ac0716cae41a909f2e4b41
Lucio <luciomoraes@gmail.com>
2024-10-09 23:21:32 +0200

    Using a StaticHandler in the controllers package

--- main.go
+++ main.go
@@ -2,52 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/luciormoraes/my-go-gallery/controllers"
 	"github.com/luciormoraes/my-go-gallery/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filePath string) {
-
-	t, err := views.Parse(filePath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-
-}
-
-func homeHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func pageNotFoundHandle(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Page not found", http.StatusNotFound)
-}
-
-func faqHandle(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandlerFunc)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandle)
-	r.NotFound(pageNotFoundHandle)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting server on :3000...")
 	http.ListenAndServe(":3000", r)
