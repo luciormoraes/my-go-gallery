b643e87e94dbfe7aac3f063a00158502809c80ed
Lucio <luciomoraes@gmail.com>
2024-12-19 19:39:30 +0100

    Created interface Template to decouple

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/luciormoraes/my-go-gallery/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template 
 	}
 }
 
